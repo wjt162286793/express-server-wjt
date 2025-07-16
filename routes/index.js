52b246d400026e074aacd1f60df3121d355f6384
wangjingtao <hju2@qq.com>
2025-07-16 19:02:29 +0800

    登录接口定义

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  console.log('进入了')
+  res.send('测试成功')
 });
 
 module.exports = router;
